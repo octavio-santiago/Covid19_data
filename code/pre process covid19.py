d245209d58979b49060016fd1244096d4ca6537c
octaviobomfim <octaviobomfim@gmail.com>
2020-03-22 14:23:22 -0300

    initial load

---
+++ code/pre process covid19.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import glob
+
+files = glob.glob(r"C:\Users\Octavio\Desktop\Projetos Python\Function Approximator\covid19\COVID-19\csse_covid_19_data\csse_covid_19_daily_reports\*.csv")
+
+for idx,file in enumerate(files):
+    if idx == 0 :
+        df = pd.read_csv(file,sep=",")
+    else:
+        df = df.append(pd.read_csv(file,sep=","))
+
+print(df.head())
+
+
+def export():
+    df.to_excel(r"C:\Users\Octavio\Desktop\Projetos Python\Function Approximator\covid19\daily cases.xlsx")
+
+    
+export()
